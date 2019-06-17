3b20e2140b32ea7889051a2418ca7ddc246c4158
wish <weishiyouxiang@126.com>
2019-06-17 18:16:56 +0800

    树木列表

--- wxsmall/pages/listtree/listtree.js
+++ wxsmall/pages/listtree/listtree.js
@@ -1,5 +1,6 @@
 // pages/listtree/listtree.js
 const app = getApp();
+const http = require("../../utils/http.js")
 const sizepage = 10
 Page({
 
@@ -11,6 +12,10 @@ Page({
     currentPage: 0,
     CustomBar: app.globalData.CustomBar,  
     listTree:[],
+    treeCategory: [{
+      id: 0,
+      name: '全部'
+    }],
     titleKey: '',
     hasdata: true,
     scrollLeft: 0
@@ -19,7 +24,19 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.search()
+    // 分类列表
+    http({
+      url: '/api/get-list',
+    }).then(res => {
+      if (res.status == 1) {
+        this.setData({
+          treeCategory: res.data.tree_category
+        })
+      }
+    }).catch(err => {
+      console.log(err)
+    })
   },
   //滑动到底部触发事件
   bindDownLoad: function () {
@@ -40,9 +57,7 @@ Page({
   loadMore() {
     this.data.currentPage++
     getCardData(this.data.currentPage, sizepage, this.data.titleKey).then(res => {
-      let newArr = res.data.map(item => {
-        return item
-      })
+      let newArr = res.data.tree_list
       this.setData({
         listTree: this.data.listTree.concat(newArr)
       })
@@ -58,9 +73,7 @@ Page({
     this.data.currentPage = 0
     getCardData(0, sizepage, this.data.titleKey).then(res => {
       console.log(res)
-      let newArr = res.data.map(item => {
-        return item
-      })
+      let newArr = res.data.tree_list
       this.setData({
         listTree: newArr,
         hasdata: true
@@ -87,6 +100,17 @@ Page({
 function getCardData(page, size, key = '') {
   console.log(page, size, key)
   return new Promise((resolve,reject)=>{
-    resolve({data:[1, 2, 3, 4]})
+    http({
+      url: '/api/tree-list',
+      data: { page, size, key }
+    }).then(res=>{
+      if(res.status==1){
+        resolve({data:res.data})
+      }else{
+        resolve({data:[]})
+      }
+    }).catch(err=>{
+      reject(err)
+    })
   })
 }
